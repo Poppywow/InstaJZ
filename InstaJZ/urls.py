3f96b7dec3aa0ec478fe59ac6e787fad76897d3f
Poppywow <wy@caoleis-macbook.lan>
2019-08-26 22:03:22 -0700

    Display static page

--- InstaJZ/urls.py
+++ InstaJZ/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from Insta.views import HelloWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HelloWorld.as_view(), name='helloworld'),
 ]
