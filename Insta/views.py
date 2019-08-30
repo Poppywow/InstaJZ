443862c12f3e7157d07089095c36a91ac4950abf
Poppywow <wy@caoleis-macbook.lan>
2019-08-30 15:43:21 -0700

    Finish website development

--- Insta/views.py
+++ Insta/views.py
@@ -1,7 +1,8 @@
+from annoying.decorators import ajax_request
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from Insta.models import Post
+from Insta.models import Post, Like, InstaUser, UserConnection
 from Insta.forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -13,10 +14,21 @@ class PostsView(ListView):
     model = Post
     template_name = 'index.html'
 
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name='user_detail.html'
 
 class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
@@ -38,3 +50,21 @@ class SignUp(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
     success_url = reverse_lazy("login")
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
